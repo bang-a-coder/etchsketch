a9c8a89fa10506578da03d69bd033db97d447900
Nikitas Filosofof <nikitas4717@gmail.com>
2020-11-11 23:45:30 +0100

    scalable table

--- script.js
+++ script.js
@@ -1,16 +1,42 @@
 const parent = document.createElement('div')
-parent.setAttribute('style', 'background-color: lightblue; width: 1280px; height: 1280px; margin: auto; display: flex;flex-direction: row;flex-wrap: wrap;justify-content: flex-start;align-items: stretch;align-content: stretch;');    
+parent.setAttribute('style', 'background-color: lightblue; width: 500px; height: 500px; margin: auto; display: flex;flex-direction: row;flex-wrap: wrap;justify-content: flex-start;align-items: stretch;align-content: center;');    
 
-function createBlock(){
-  const block = document.createElement('div')
 
-  block.setAttribute('style', 'width: 80px; height: 80px; background: red;')
+
+let tableside = 16
+
+function createBlock(tableside){
+  let block = document.createElement('div')
+  
   parent.appendChild(block)
+
+  let divsize = 500 / tableside
+  style(block, 'red',divsize)
+
+
+  block.addEventListener('mouseenter', e => {
+    style(block,"grey",divsize)
+  });
+
+  block.addEventListener('mouseleave', e => {
+    style(block, 'red', divsize)
+  });
 }
 
-for (i = 1; i <= 32; i++){
-  createBlock()
+function style(block,color, divsize) {
+  block.setAttribute('style', `background: ${color};`)
+  block.style.width = divsize.toString() + 'px'
+  block.style.height = divsize.toString() + 'px'
+}
+
+for (i = 1; i <= tableside ** 2; i++){
+  createBlock(tableside)
 }
 
 document.body.appendChild(parent)
 
+let dsfg = document.se
+
+
+
+let inputDiv = document.
