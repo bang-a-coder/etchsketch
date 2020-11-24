e0d5cfd869b6dd086d9b38a7521af7af13a8720a
Nikitas Filosofof <nikitas4717@gmail.com>
2020-11-24 20:59:26 +0100

    clean code

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 const parent = document.createElement('div')
-  parent.setAttribute('style', 'background-color: lightblue; width: 500px; height: 500px; margin: auto; display: flex;flex-direction: row;flex-wrap: wrap;justify-content: flex-start;align-items: stretch;align-content: center;');    
-
-
+parent.setAttribute('style', 'background-color: lightblue; width: 500px; height: 500px; margin: auto; display: flex;flex-direction: row;flex-wrap: wrap;justify-content: flex-start;align-items: stretch;align-content: center;');    
 
 const inputDaddy = document.createElement('div')
-  inputDaddy.setAttribute('style', 'background-color: green; width: 300px; height: 100px; margin: auto; display: flex;flex-direction: row;flex-wrap: wrap;justify-content: flex-start;align-items: stretch;align-content: center;');    
+inputDaddy.setAttribute('style', 'background-color: green; width: 300px; height: 100px; margin: auto; display: flex;flex-direction: row;flex-wrap: wrap;justify-content: flex-start;align-items: stretch;align-content: center;');    
 
 document.body.appendChild(inputDaddy)
 
@@ -22,31 +20,20 @@ let btnDelete = document.createElement("button");
 
 inputDaddy.appendChild(btnDelete)
 
-
-
-
-
-
-
-
 function createBlock(tableside){
   let block = document.createElement('div')
   block.classList.add("divblock");
 
-
   let divsize = 500 / tableside
 
   block.setAttribute('style', `background: red; width: ${divsize.toString() + 'px'}; height: ${divsize.toString() + 'px'};`)
+  block.addEventListener('mouseenter', e => {
+    block.style.background = `gray`
+    }
+  )  
 
   parent.appendChild(block)
 
-  block.addEventListener('mouseenter', e => {
-    block.style.background = `gray`
-
-})  
-
 }
-
-
 
 document.body.appendChild(parent)
